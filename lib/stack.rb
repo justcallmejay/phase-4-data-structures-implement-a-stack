b828e3861bf10c1f3389da498bd8c2edddc56902
jay <jaypifiverr@gmail.com>
2023-07-18 17:56:06 -0400

    commit tree d87839f050f4f265f2eaa729001f44ea5ef4de1e

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,21 @@
 # your code here
+class Stack
+
+    def initialize
+        @stack = []
+    end
+
+    def push(foo)
+        @stack.push(foo)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+
+end
